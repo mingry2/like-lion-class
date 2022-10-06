7b9b50b75142c415dc65eefc13f5c6f562d21224
minkyoung kim <minry20415@naver.com>
2022-10-06 15:16:58 +0900

    [Update] 컴파일 결과 주석 달기 --mk

--- src/studyclass/java_1006/readfile/ReadFile.java
+++ src/studyclass/java_1006/readfile/ReadFile.java
@@ -19,6 +19,20 @@ public class ReadFile {
         for(File file : files){
             System.out.println(file);
         }
+/*
+"C:\Program Files\Java\jdk1.8.0_341\bin\java.exe" "-javaagent:D:\IntelliJ IDEA Community Edition 2022.2.1\lib\idea_rt.jar=61217:D:\IntelliJ IDEA Community Edition 2022.2.1\bin" -Dfile.encoding=UTF-8 -classpath "C:\Program Files\Java\jdk1.8.0_341\jre\lib\charsets.jar;C:\Program Files\Java\jdk1.8.0_341\jre\lib\deploy.jar;C:\Program Files\Java\jdk1.8.0_341\jre\lib\ext\access-bridge-64.jar;C:\Program Files\Java\jdk1.8.0_341\jre\lib\ext\cldrdata.jar;C:\Program Files\Java\jdk1.8.0_341\jre\lib\ext\dnsns.jar;C:\Program Files\Java\jdk1.8.0_341\jre\lib\ext\jaccess.jar;C:\Program Files\Java\jdk1.8.0_341\jre\lib\ext\jfxrt.jar;C:\Program Files\Java\jdk1.8.0_341\jre\lib\ext\localedata.jar;C:\Program Files\Java\jdk1.8.0_341\jre\lib\ext\nashorn.jar;C:\Program Files\Java\jdk1.8.0_341\jre\lib\ext\sunec.jar;C:\Program Files\Java\jdk1.8.0_341\jre\lib\ext\sunjce_provider.jar;C:\Program Files\Java\jdk1.8.0_341\jre\lib\ext\sunmscapi.jar;C:\Program Files\Java\jdk1.8.0_341\jre\lib\ext\sunpkcs11.jar;C:\Program Files\Java\jdk1.8.0_341\jre\lib\ext\zipfs.jar;C:\Program Files\Java\jdk1.8.0_341\jre\lib\javaws.jar;C:\Program Files\Java\jdk1.8.0_341\jre\lib\jce.jar;C:\Program Files\Java\jdk1.8.0_341\jre\lib\jfr.jar;C:\Program Files\Java\jdk1.8.0_341\jre\lib\jfxswt.jar;C:\Program Files\Java\jdk1.8.0_341\jre\lib\jsse.jar;C:\Program Files\Java\jdk1.8.0_341\jre\lib\management-agent.jar;C:\Program Files\Java\jdk1.8.0_341\jre\lib\plugin.jar;C:\Program Files\Java\jdk1.8.0_341\jre\lib\resources.jar;C:\Program Files\Java\jdk1.8.0_341\jre\lib\rt.jar;D:\workspace\git\likelionstudy01\out\production\likelionstudy01" studyclass.java_1006.readfile.ReadFile
+.\.git
+.\.git
+.\.idea
+.\a_file.txt
+.\likelionstudy01.iml
+.\out
+.\README.md
+.\src
+
+Process finished with exit code 0
+
+*/
     }
 
 }
