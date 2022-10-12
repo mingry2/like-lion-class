c6f89a4dc826dd9e60c7a31b11ff77eb708cebff
minkyoung kim <minry20415@naver.com>
2022-10-12 17:33:31 +0900

    [add] babalikelion - GuguDan

---
+++ src/babalion/day3/GuguDan.java
@@ -0,0 +1,12 @@
+package babalion.day3;
+
+public class GuguDan {
+    public static void main(String[] args) {
+
+        for(int i = 2; i <= 9; i++){
+            for(int j = 1; j <= 9; j++){
+                System.out.println(i + "*" + j + "=" + i*j);
+            }
+        }
+    }
+}
