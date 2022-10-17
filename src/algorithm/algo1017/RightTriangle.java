c981a2a61ccc16170b7826323d0c8d496cd5c504
minkyoung kim <minry20415@naver.com>
2022-10-17 09:26:19 +0900

    [add] 알고리즘 RightTriangle(메서드 사용)

--- src/algorithm/algo1017/RightTriangle.java
+++ src/algorithm/algo1017/RightTriangle.java
@@ -1,10 +1,17 @@
 package algorithm.algo1017;
 
 public class RightTriangle {
+    public void star(int num) {
+        for(int i = 0; i < num; i++){
+            for(int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
+        RightTriangle rt = new RightTriangle();
+        rt.star(4);
     }
 }
